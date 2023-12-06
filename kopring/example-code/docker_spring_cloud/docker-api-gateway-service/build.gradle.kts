fe6c494d6992de439628b5ea9783af19366f28e4
chagchagchag <chagchagchag.dev@gmail.com>
2023-12-07 01:34:18 +0900

    spring-cloud 를 docker 환경에서 구성하는 예제프로젝트

--- kopring/example-code/docker_spring_cloud/docker-api-gateway-service/build.gradle.kts
+++ kopring/example-code/docker_spring_cloud/docker-api-gateway-service/build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    id("com.google.cloud.tools.jib") version "3.4.0"
     id("org.springframework.boot") version "3.2.0"
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.20"
@@ -42,4 +43,49 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+jib {
+    val profile : String = project.findProperty("env") as? String ?: "local"
+
+    from {
+        image = "amazoncorretto:17"
+
+        // M1 Mac을 사용할 경우 아래 내용을 추가.
+//            platforms {
+//                platform {
+//                    architecture = "arm64"
+//                    os = "linux"
+//                }
+//            }
+    }
+
+    to {
+        image = "chagchagchag/docker-api-gateway-service"
+        // image tag 는 여러개 지정 가능하다.
+        tags = setOf("latest")
+    }
+
+    container{
+        creationTime = "USE_CURRENT_TIMESTAMP"
+
+        // jvm 옵션
+        jvmFlags = listOf(
+            "-Dspring.profiles.active=${profile}",
+            "-XX:+UseContainerSupport",
+            "-Dserver.port=8000",
+            "-Dfile.encoding=UTF-8",
+        )
+
+        // 컨테이너 입장에서 외부로 노출할 포트
+        ports = listOf("8000")
+
+        labels = mapOf(
+            "maintainer" to "chagachagchag.dev@gmail.com"
+        )
+    }
+
+    extraDirectories{
+
+    }
 }
